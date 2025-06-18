0aef8d58976f919924199f0f9eb22d2de7050f53
Tejasvini Kumawat <tejasvinikumawat2004@gmail.com>
2025-06-18 23:20:04 +0530

    ready for Deployments

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,12 +6,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(`${process.env.MONGODB_URI}/SheFlowsDB`);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("Database connection error:", error);
+    console.error("❌ MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
